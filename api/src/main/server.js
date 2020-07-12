508b4cafb8d7f4f12af1562bcc02abad079e2a8a
lesimoes <lesimoes0@gmail.com>
2020-07-12 18:14:06 -0300

    minor fixes

--- api/src/main/server.js
+++ api/src/main/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const app = require('../config/app');
 
-app.listen(3000, () => console.log('Magic happnes on 3000'));
+app.listen(process.env.API_PORT || 3000, () => console.log('Magic happnes on 3000'));
